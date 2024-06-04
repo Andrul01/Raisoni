a1b3fe229cfe5c7225504eb1fb1506ae23105129
Andrul01 <120774561+Andrul01@users.noreply.github.com>
2024-06-04 12:24:21 +0000

    .js file change

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-console.log("Name:Andrul")
+ 
 
-console.log("username:Andrul01")
+// Add this script to your HTML file to make the website responsive.
+// This script makes the navigation menu collapse on smaller screens.
+const navMenu = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
